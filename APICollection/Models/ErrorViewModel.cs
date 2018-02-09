fc1436ab4cedf47e272763497127aff2f855e858
kwa <kwatran@gmail.com>
2018-02-09 17:59:04 +0900

    Add project files.

---
+++ APICollection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APICollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
